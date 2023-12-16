33a8dbdf6c3748f532f78e0391b9cf94ed0b70ec
HerbertHe <Herbert.He0229@gmail.com>
2023-12-16 17:26:23 +0800

    :tada:

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # iptv-sources-local-checker
+
 Local checker for IPTV sources
+
+> **WIP⚠️** Work In Progress
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/renderer/index.html', './src/**/*.{js,ts,jsx,tsx}'],
+
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
